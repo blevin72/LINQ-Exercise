7bfe5c95b5781d012151d8def05f0ee34a175906
Bryan Levin <bryanlevin72@gmail.com>
2023-07-11 12:06:03 -0700

    Exercise 2

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace LINQ
+﻿using System.Linq;
+using System.Collections.Generic;
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGames = new List<string>() { "Starcraft", "Madden", "Halo", "Last of Us" };
+            videoGames.Add("Call of Duty");
+            videoGames.Add("God of War");
+
+            videoGames.OrderBy(videoGames => videoGames.Length).ToList().ForEach(videoGames => Console.WriteLine(videoGames));
         }
     }
 }
